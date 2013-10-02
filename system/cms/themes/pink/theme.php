e6a8eb2697955e017d9beb4d0b6af556de8ea32f
Don Gilbert <don@electriceasel.com>
2013-10-02 09:26:24 -0500

    first go at the pink theme

---
+++ system/cms/themes/pink/theme.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Pink extends Theme {
+
+    public $name			= 'Pink Theme';
+    public $author			= 'Amber Wheet';
+    public $author_website	= 'http://electriceasel.com/';
+    public $website			= 'http://electriceasel.com/';
+    public $description		= 'Pink Theme for AdoptSites.';
+    public $version			= '1.0.0';
+
+}
+
+/* End of file theme.php */
