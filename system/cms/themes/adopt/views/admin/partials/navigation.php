21788380b0c021f9f95dcd125f84fe5a44194820
Don Gilbert <don@electriceasel.com>
2013-10-07 16:59:16 -0500

    Updating sidebar layout / colors

--- system/cms/themes/adopt/views/admin/partials/navigation.php
+++ system/cms/themes/adopt/views/admin/partials/navigation.php
@@ -1,7 +1,7 @@
-<div class="well" style="padding:9px 0">
+<div class="well" style="padding:0">
 	<ul class="nav nav-list">
 
-		<li><?php echo anchor('admin', lang('global:dashboard'), 'class="top-link '.( ! $this->module ? 'active ' : '').'"') ?></li>
+		<li class="<?php echo !$this->module ? ' active' : ''; ?>"><?php echo anchor('admin', '<i class="icon icon-home"></i>'.lang('global:dashboard')) ?></li>
 		<?php
 		// Display the menu items.
 		// We have already vetted them for permissions
@@ -9,13 +9,15 @@
 		// display them now.
 		foreach ($menu_items as $key => $menu_item)
 		{
+			$class = strtolower($key);
 			if (is_array($menu_item))
 			{
-				echo '<li class="nav-header">'.lang_label($key).'</li>';
+				echo '<li class="nav-header section-'.$class.'">'.lang_label($key).'</li>';
 
 				foreach ($menu_item as $lang_key => $uri)
 				{
-					echo '<li><a href="'.site_url($uri).'" class="">'.lang_label($lang_key).'</a></li>';
+					$itemClass = str_replace(' ', '-', $class.'-'.strtolower(lang_label($lang_key)));
+					echo '<li><a href="'.site_url($uri).'" class="'.$itemClass.'"><i></i>'.lang_label($lang_key).'</a></li>';
 
 				}
 			}
@@ -23,12 +25,14 @@
 			{
 				foreach ($menu_item as $lang_key => $uri)
 				{
-					echo '<li><a href="'.site_url($menu_item).'" class="top-link no-submenu">'.lang_label($lang_key).'</a></li>';
+					$itemClass = str_replace(' ', '-', $class.'-'.strtolower(lang_label($lang_key)));
+					echo '<li><a href="'.site_url($menu_item).'" class="no-submenu '.$itemClass.'"><i></i>'.lang_label($lang_key).'</a></li>';
 				}
 			}
 			elseif (is_string($menu_item))
 			{
-				echo '<li><a href="'.site_url($menu_item).'" class="top-link no-submenu">'.lang_label($key).'</a></li>';
+				$itemClass = str_replace(' ', '-', $class.'-'.strtolower(lang_label($key)));
+				echo '<li><a href="'.site_url($menu_item).'" class="no-submenu '.$itemClass.'"><i></i>'.lang_label($key).'</a></li>';
 			}
 
 		}
